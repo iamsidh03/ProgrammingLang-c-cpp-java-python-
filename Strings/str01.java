006339a30158b25ba8114372a45f4b1700c2111f
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2025-02-08 15:31:38 +0000

    rge

---
+++ Strings/str01.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        char arr[]={'a','b'};
+        String str="abc";
+        String str2=new String("xyz");
+        // 'str' and 'str2' both are strings butImpelemention level of str and str2 is different
+        //Strings are IMMutable in java
+        Scanner sc =new Scanner(System.in);
+        String name;
+        name=sc.next();
+        Syatem.out.println(name);
+    }
+}
