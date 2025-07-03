ea7935d6cfdd2716bf93805afa13152fc265671c
Siddharth <siddharthraj3101@gmail.com>
2025-07-04 02:03:44 +0530

    deleteAtPosition

--- Dsa_cpp/LinkedList/insert_delete.cpp
+++ Dsa_cpp/LinkedList/insert_delete.cpp
@@ -1,108 +1,190 @@
 #include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node() {
+    Node()
+    {
         this->data = 0;
         this->next = NULL;
     }
 
-    Node(int value) {
+    Node(int value)
+    {
         this->data = value;
         this->next = NULL;
     }
+    // destructutor to delete a node
+    /* ~Node() {
+        if (next != NULL) {
+            delete next; // Recursively delete the next node
+            next = NULL; // Set next to NULL to avoid dangling pointer
+        }
+        cout << "Node with data " << data << " deleted." << endl;
+    } */
+
+    ~Node()
+    {
+        cout << "Node with data " << data << " deleted." << endl;
+    }
 };
 
-int findLength(Node* &head){
-    int len=0;
-    Node* temp=new Node;
-    while(temp!=NULL){
+int findLength(Node *&head)
+{
+    int len = 0;
+    Node *temp = new Node;
+    while (temp != NULL)
+    {
         len++;
-        temp=temp->next;
+        temp = temp->next;
     }
     return len;
 }
 
-void insertAtHead(int data, Node*& head, Node*& tail) {
-    if(head==NULL){
-    Node* newNode = new Node(data);
-    head = tail = newNode;
-    return;
-    }
-   /*  // if list is empty
-    if (head == NULL) {
+void insertAtHead(int data, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
         head = tail = newNode;
         return;
-    } */
-    Node* newNode = new Node(data);
+    }
+    /*  // if list is empty
+     if (head == NULL) {
+         head = tail = newNode;
+         return;
+     } */
+    Node *newNode = new Node(data);
     newNode->next = head;
     head = newNode;
 }
 
-void insertAtTail(int data, Node*& head, Node*& tail) {
+void insertAtTail(int data, Node *&head, Node *&tail)
+{
 
-      if(head==NULL){
-    Node* newNode = new Node(data);
-    head = tail = newNode;
-    return;
-    }
-
-   /*  // if list is empty
-    if (tail == NULL) {
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
         head = tail = newNode;
         return;
-    } */
-    Node* newNode = new Node(data);
+    }
+
+    /*  // if list is empty
+     if (tail == NULL) {
+         head = tail = newNode;
+         return;
+     } */
+    Node *newNode = new Node(data);
     tail->next = newNode;
     tail = newNode;
 }
-void inserAtposition(int data,int position,Node* &head,Node* &tail){
-   
-    if(head==NULL){ //if list is empty
-        Node* newNode=new Node(data);
-        head=tail=newNode;
+void insertAtposition(int data, int position, Node *&head, Node *&tail)
+{
+
+    if (head == NULL)
+    { // if list is empty
+        Node *newNode = new Node(data);
+        head = tail = newNode;
         return;
     }
-    if(position==0){//insert at head
-        insertAtHead(data,head,tail);
+    if (position == 0)
+    { // insert at head
+        insertAtHead(data, head, tail);
         return;
     }
-    int len=findLength(head);
-    if(position>len){  //if position is greater than length of list, insert at tail
+    int len = findLength(head);
+    if (position > len)
+    { // if position is greater than length of list, insert at tail
         insertAtTail(data, head, tail);
         return;
     }
-    int i=1;
-    Node* prev=head;
-    while(i<position && head!=NULL){ //insert at any position in between
-        prev=prev->next;
+    int i = 1;
+    Node *prev = head;
+    while (i < position && head != NULL)
+    { // insert at any position in between
+        prev = prev->next;
         i++;
     }
-    Node* curr= prev->next;
+    Node *curr = prev->next;
 
-    Node* newNode = new Node(data);
-    newNode->next=curr;
-    prev->next=newNode;
-
+    Node *newNode = new Node(data);
+    newNode->next = curr;
+    prev->next = newNode;
 }
 
-void print(Node*& head) {
-    Node* temp = head;
-    while (temp != NULL) {
+void print(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
         cout << temp->data << " ";
         temp = temp->next;
     }
     cout << endl;
 }
+void deleteAtPosition(int position, Node *&head, Node *&tail)
+{
+    if (head == NULL)
+    {
+        cout << "List is empty, nothing to delete." << endl;
+        return;
+    }
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
+    int len = findLength(head);
+    if(position>len){
+        cout<<"Position is greater than length of list, nothing to delete."<<endl;
+        return;
+    }
+    if (position == len)
+    {
+        int i = 1;
+        // step1:find previous node
+        Node *prev = head;
+        while (i < len - 1)
+        {
+            prev = prev->next;
+            i++;
+        }
+        // step2: prev-Next=NULL;
+        prev->next = NULL;
+        Node *temp = tail;
 
+        tail = prev;
+        // step3: delete tail
+        delete temp;
+        return;
+    }
 
-int main() {
-    Node* head = NULL;
-    Node* tail = NULL;
+    //delete at middle
+    int i=1;
+    Node* prev=head;
+    while(i<position-1 && prev!=NULL)
+    {
+        prev=prev->next;
+        i++;
+    }
+    Node* curr=prev->next;
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
+    return;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
 
     // Instead of assigning manually to head->next (head is NULL), insert using function
     insertAtTail(20, head, tail);
@@ -113,15 +195,17 @@ int main() {
     insertAtHead(15, head, tail);
     insertAtHead(25, head, tail);
     insertAtHead(35, head, tail);
-     cout << "Printing the Linked List:" << endl;
+    cout << "Printing the Linked List:" << endl;
+    print(head);
+    cout << endl;
+    insertAtposition(100, 7, head, tail);
+
+    cout << "Printing the Linked List after insert at position call:" << endl;
     print(head);
     cout<<endl;
-    inserAtposition(100, 7, head, tail);
-
-     cout << "Printing the Linked List after insert at position call:" << endl;
+    deleteAtPosition(9, head, tail);
+    // cout << "deleted at position call " << endl;
     print(head);
-  
-   
 
     return 0;
 }
